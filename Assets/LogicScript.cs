0aa4dd7bf7698c1f670dc004f0d79c4799735236
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-07-26 22:31:54 +0700

    Add Logic Management

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class LogicScript : MonoBehaviour
+{
+
+    public float score;
+    private float highScore;
+    public GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("Score : " + score);
+    }
+
+    public void addScore(float scoreTodAdd)
+    {
+        score += scoreTodAdd;
+    }
+
+    public void gameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
