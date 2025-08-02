fbdf902c0b55a1811899b4306335a7c75748dbd1
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-08-02 19:11:24 +0700

    added swinging rope mechanism

--- Assets/Scripts/PlayerColiisonScript.cs
+++ Assets/Scripts/PlayerColiisonScript.cs
@@ -8,6 +8,20 @@ public class PlayerColiison : MonoBehaviour
     private float horizontalInput;
     private float playerStep;
     private ParallaxBackground[] allParallax;
+    private HingeJoint2D playerJoint;
+    private Rigidbody2D rb;
+    private Animator playerAnimation;
+
+    private bool canGrabRope = true;
+    private float ropeCooldown = 0.5f;
+    private float ropeCooldownTimer = 0f;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+    }
 
     private void Awake()
     {
@@ -31,6 +45,29 @@ public class PlayerColiison : MonoBehaviour
                 logicScript.addScore(10);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnimation.SetBool("isSwing", false);
+            HingeJoint2D joint = GetComponent<HingeJoint2D>();
+            if (joint != null)
+            {
+                Destroy(joint);
+                playerJoint = null;
+                canGrabRope = false;
+            }
+        }
+
+        if (!canGrabRope)
+        {
+            ropeCooldownTimer += Time.deltaTime;
+            if (ropeCooldownTimer >= ropeCooldown)
+            {
+                canGrabRope = true;
+                ropeCooldownTimer = 0f;
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -48,6 +85,22 @@ public class PlayerColiison : MonoBehaviour
             foreach (ParallaxBackground pb in allParallax)
             {
                 pb.ResetParallax();
+            }
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("RopeSegment") && canGrabRope)
+        {
+            if (GetComponent<HingeJoint2D>() == null)
+            {
+                playerAnimation.SetBool("isSwing", true);
+                playerJoint = gameObject.AddComponent<HingeJoint2D>();
+                playerJoint.connectedBody = collision.rigidbody;
+                playerJoint.autoConfigureConnectedAnchor = false;
+                playerJoint.anchor = Vector2.zero;
+                playerJoint.connectedAnchor = Vector2.zero;
             }
         }
     }
